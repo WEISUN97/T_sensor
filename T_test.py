9fd4fd5d9a9ba0a33867e160d76f9f47d6241e07
WEISUN97 <wsun7@binghamton.edu>
2025-11-04 10:16:31 -0600

    feat: update

--- T_test.py
+++ T_test.py
@@ -8,7 +8,7 @@ L = [2000]
 w = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 2.0, 3.0, 4.0, 5.0]
 for i in range(len(L)):
     for j in range(len(w)):
-        beam = gf.components.rectangle(size=(L[i], w[j]), layer=(1, 0))
+        beam = gf.components.rectangle(size=(L[i], w[j]), layer=(2, 0))
         beam_ref = cell << beam
         T = gf.components.text(f"{w[j]}", size=10, layer=(1, 0))
         T_ref1 = cell << T
@@ -26,7 +26,7 @@ L = [500]
 w = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 2.0, 3.0, 4.0, 5.0]
 for i in range(len(L)):
     for j in range(len(w)):
-        beam = gf.components.rectangle(size=(L[i], w[j]), layer=(1, 0))
+        beam = gf.components.rectangle(size=(L[i], w[j]), layer=(2, 0))
         beam_ref = cell << beam
         T = gf.components.text(f"{w[j]}", size=10, layer=(1, 0))
         T_ref1 = cell << T
@@ -42,9 +42,10 @@ T_ref.move((0, -400))
 # Part 3 L=1000um
 L = [1000]
 w = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 2.0, 3.0, 4.0, 5.0]
+
 for i in range(len(L)):
     for j in range(len(w)):
-        beam = gf.components.rectangle(size=(L[i], w[j]), layer=(1, 0))
+        beam = gf.components.rectangle(size=(L[i], w[j]), layer=(2, 0))
         beam_ref = cell << beam
         T = gf.components.text(f"{w[j]}", size=10, layer=(1, 0))
         T_ref1 = cell << T
@@ -56,8 +57,19 @@ for i in range(len(L)):
 T3 = gf.components.text(f"L=1000", size=20, layer=(1, 0))
 T_ref = cell << T3
 T_ref.move((600, -400))
-cell.flatten()
 
+frame1 = gf.components.rectangle(size=(2000, 285), layer=(3, 0))
+(cell << frame1).movey(-10)
+frame2 = gf.components.rectangle(size=(500, 280.1), layer=(3, 0))
+(cell << frame2).movey(-370)
+frame3 = gf.components.rectangle(size=(1000, 280.1), layer=(3, 0))
+(cell << frame3).move((640, -370))
+
+beams = gf.boolean(
+    A=cell, B=cell, operation="not", layer1=(3, 0), layer2=(2, 0), layer=(1, 0)
+)
+cell << beams
+cell.flatten()
 # cell.show()
 suffix = datetime.now().strftime("%Y%m%d_%H")
 cell.write_gds(f"T_test{suffix}.gds", write_context_info=False)
