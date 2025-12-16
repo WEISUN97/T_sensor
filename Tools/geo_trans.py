672eec295ae994d800bf6d63750969a4985845d3
WEISUN97 <wsun7@binghamton.edu>
2025-12-16 16:27:15 -0600

    feat: update

--- Tools/geo_trans.py
+++ Tools/geo_trans.py
@@ -11,3 +11,26 @@ def polygon(points):
 def region(p):
     r = gf.kdb.Region(p.to_itype(gf.kcl.dbu))
     return r
+
+
+# Define a custom polynomial transition function from y1 -> y2, for t ∈ [0,1].
+def polynomial(t: float, y1: float, y2: float) -> float:
+    return (y2 - y1) * t**3 + y1
+
+
+# round corner
+def round_corner(w1: float, w2: float, length: float, rotation=0, layer=(2, 0)):
+    cs1 = gf.get_cross_section("strip", width=w1, layer=layer)
+    cs2 = gf.get_cross_section("strip", width=w2, layer=layer)
+
+    transition = gf.path.transition(cs1, cs2, width_type="parabolic")
+    p = gf.path.straight(length, npoints=100)
+    c = gf.path.extrude_transition(p, transition)
+    c_component = gf.Component()
+    (c_component << c).rotate(angle=rotation)
+    return c_component
+
+
+if __name__ == "__main__":
+    c_component = round_corner(6, 2, 2, rotation=90)
+    c_component.show()
